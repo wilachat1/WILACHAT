c7b32bbcb3f07c37e6410d83db10dc0a29dfff3d
WILACHAT <waanwaanwilachat@hotmail.com>
2019-06-22 09:52:56 +0700

    new project

---
+++ SENIOR PROJECT/SENIOR PROJECT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SENIOR PROJECT
+//
+//  Created by WILACHAT on 22/6/2562 BE.
+//  Copyright © 2562 WILACHAT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SENIOR PROJECT/SENIOR PROJECTTests/SENIOR_PROJECTTests.swift
@@ -0,0 +1,34 @@
+//
+//  SENIOR_PROJECTTests.swift
+//  SENIOR PROJECTTests
+//
+//  Created by WILACHAT on 22/6/2562 BE.
+//  Copyright © 2562 WILACHAT. All rights reserved.
+//
+
+import XCTest
+@testable import SENIOR_PROJECT
+
+class SENIOR_PROJECTTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
