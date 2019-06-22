5e0be5f448128a23af2d8cce5ccc2a55b86fc815
WILACHAT <waanwaanwilachat@hotmail.com>
2019-06-22 12:47:10 +0700

    function username password true or false

--- SENIOR PROJECT/SENIOR PROJECT/ViewController.swift
+++ SENIOR PROJECT/SENIOR PROJECT/ViewController.swift
@@ -10,11 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var coverImage: UIImageView!
+    @IBOutlet weak var usernameInput: UITextField!
+    @IBOutlet weak var passwordInput: UITextField!
+    
+    @IBOutlet weak var loginInsert: UIButton!
+    
+    @IBOutlet weak var forgotPasswordInput: UIButton!
+    
+    @IBOutlet weak var backInput: UIButton!
+    let a = "Wilachat"
+    let b = "Weesakul"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func loginHandler(_ sender: Any) {
+        let username = usernameInput.text ?? "User"
+        let password = passwordInput.text ?? "Pass"
+         print("\(username)::\(password)")
+    }
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+//        let username =
+//        let password =
+        return checkUsernameAndPassword(username: usernameInput.text ?? "User", password: passwordInput.text ?? "Pass")
+//        return passed
+    }
+    func checkUsernameAndPassword (username: String, password: String) -> Bool {
+        //do something
+        if username == a && password == b {
+            return true
+        }
+        
+        return false
+        
+    }
+    
 }
 
