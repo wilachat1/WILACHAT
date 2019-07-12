6573b6b4352d905deb6e5537b0a5b70deb2ec836
WILACHAT <waanwaanwilachat@hotmail.com>
2019-07-12 21:06:06 +0700

    ไปไม่ถึงไหนทําไม่ค่อยได้....

---
+++ SENIOR PROJECT/SENIOR PROJECT/HighScoreHeaderTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HighScoreHeaderTableViewCell.swift
+//  SENIOR PROJECT
+//
+//  Created by WILACHAT on 12/7/2562 BE.
+//  Copyright © 2562 WILACHAT. All rights reserved.
+//
+
+import UIKit
+
+class HighScoreHeaderTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ SENIOR PROJECT/SENIOR PROJECT/HighScoreNameTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HighScoreNameTableViewCell.swift
+//  SENIOR PROJECT
+//
+//  Created by WILACHAT on 12/7/2562 BE.
+//  Copyright © 2562 WILACHAT. All rights reserved.
+//
+
+import UIKit
+
+class HighScoreNameTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
