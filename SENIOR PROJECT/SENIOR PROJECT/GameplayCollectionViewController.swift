e0e0c3bfeb79e121be2b6686ed5b826296d35e08
WILACHAT <waanwaanwilachat@hotmail.com>
2019-07-13 13:31:44 +0700

    [WIP] Circle layout and Random logic

---
+++ SENIOR PROJECT/SENIOR PROJECT/GameplayCollectionViewController.swift
@@ -0,0 +1,94 @@
+//
+//  GameplayCollectionViewController.swift
+//  SENIOR PROJECT
+//
+//  Created by WILACHAT on 13/7/2562 BE.
+//  Copyright © 2562 WILACHAT. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "gameItemsID"
+
+class GameplayCollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Register cell classes
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 8
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        
+    
+        // Configure the cell
+        cell.contentView.layer.cornerRadius = cell.contentView.bounds.height/2
+        let red = CGFloat(arc4random() % 256)/255.0
+        let green = CGFloat(arc4random() % 256)/255.0
+        let blue = CGFloat(arc4random() % 256)/255.0
+        cell.contentView.backgroundColor = UIColor.init(red: red, green: green, blue: blue, alpha: 1)
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+    
+    }
+    */
+
+}
---
+++ SENIOR PROJECT/SENIOR PROJECT/RandomManager.swift
@@ -0,0 +1,57 @@
+//
+//  RandomManager.swift
+//  SENIOR PROJECT
+//
+//  Created by WILACHAT on 13/7/2562 BE.
+//  Copyright © 2562 WILACHAT. All rights reserved.
+//
+
+import UIKit
+
+class RandomManager: NSObject {
+    let shared = RandomManager()
+    var listChoice = [Bool]()
+    var numOfCorrect: Int = 0
+    var numOfChoice: Int = 0
+    var correctPercentage: Double = 0//0-100
+    
+    override init() {
+        numOfChoice = 10
+        numOfCorrect = 10
+        correctPercentage = Double(numOfChoice/numOfCorrect)
+        createRandomObject()
+        
+        print (listChoice)
+    }
+    
+    func randomFromPercent(percent: Double) -> Bool {
+        let chance:Double = 1/percent
+        let decimal = round(100*(chance))/100
+        let modNumber: UInt32 = UInt32(Int(decimal * 100))
+        print("arc4random() % \(modNumber) == 0")
+        return arc4random() % modNumber == 0
+    }
+    
+    func createRandomObject(){
+        for _ in 0..<numOfChoice {
+            listChoice.append(false)
+        }
+        
+        print (listChoice)
+        var index:Int = 0
+        var correctIndex:Int = 0
+        while correctIndex < numOfCorrect{
+            
+            if randomFromPercent(percent: correctPercentage) && !listChoice[index] {
+                listChoice[index] = true
+                correctIndex += 1
+            }
+            
+            index += 1
+            if index >= numOfChoice {
+                index = 0
+            }
+        }
+    }
+
+}
